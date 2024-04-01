d6ad93ab8f4a78b695af004dadb4f9cffe13e508
DayanithiMaran <164194321+DayanithiMaran@users.noreply.github.com>
2024-04-01 19:30:58 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,463 @@
+
+
+var score=1;
+
+// -----------------------firstquestion----------------------------------
+function firstquiz1(){
+   
+
+    document.getElementById('btn-1').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-3').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-3').disabled=true;
+    document.getElementById('btn-1').disabled=true;
+    document.getElementById('btn-2').disabled=true;
+    document.getElementById('btn-4').disabled=true;
+    
+ 
+}
+function firstquiz2(){
+
+    document.getElementById('btn-2').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-3').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-3').disabled=true;
+    document.getElementById('btn-1').disabled=true;
+    document.getElementById('btn-2').disabled=true;
+    document.getElementById('btn-4').disabled=true;
+ 
+}
+function firstquiz3(){
+
+    document.getElementById('btn-3').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-3').disabled=true;
+    document.getElementById('btn-1').disabled=true;
+    document.getElementById('btn-2').disabled=true;
+    document.getElementById('btn-4').disabled=true;
+  
+        document.getElementById('scores').textContent=score++;
+  
+}
+function firstquiz4(){
+
+    document.getElementById('btn-4').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-3').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-3').disabled=true;
+    document.getElementById('btn-1').disabled=true;
+    document.getElementById('btn-2').disabled=true;
+    document.getElementById('btn-4').disabled=true;
+  
+}
+
+// -----------------------------------secondquestion---------------------
+
+function firstquiz5(){
+   
+
+    document.getElementById('btn-5').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-8').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-5').disabled=true;
+    document.getElementById('btn-6').disabled=true;
+    document.getElementById('btn-7').disabled=true;
+    document.getElementById('btn-8').disabled=true;
+    
+ 
+}
+function firstquiz6(){
+
+    document.getElementById('btn-6').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-8').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-5').disabled=true;
+    document.getElementById('btn-6').disabled=true;
+    document.getElementById('btn-7').disabled=true;
+    document.getElementById('btn-8').disabled=true;
+ 
+}
+function firstquiz7(){
+
+    document.getElementById('btn-8').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-7').style.background=" rgb(248, 68, 68)";
+
+    document.getElementById('btn-8').disabled=true;
+    document.getElementById('btn-').disabled=true;
+    document.getElementById('btn-').disabled=true;
+    document.getElementById('btn-').disabled=true;
+  
+  
+}
+function firstquiz8(){
+
+    document.getElementById('btn-8').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-5').disabled=true;
+    document.getElementById('btn-6').disabled=true;
+    document.getElementById('btn-7').disabled=true;
+    document.getElementById('btn-8').disabled=true;
+    document.getElementById('scores').textContent=score++;
+
+}
+
+// ------------------------thirdquestion-------------------
+function firstquiz9(){
+   
+
+    document.getElementById('btn-9').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-9').disabled=true;
+    document.getElementById('btn-10').disabled=true;
+    document.getElementById('btn-11').disabled=true;
+    document.getElementById('btn-12').disabled=true;
+    
+    document.getElementById('scores').textContent=score++;
+
+}
+function firstquiz10(){
+
+    document.getElementById('btn-10').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-9').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-9').disabled=true;
+    document.getElementById('btn-10').disabled=true;
+    document.getElementById('btn-11').disabled=true;
+    document.getElementById('btn-12').disabled=true;
+ 
+}
+function firstquiz11(){
+
+    document.getElementById('btn-11').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-9').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-9').disabled=true;
+    document.getElementById('btn-10').disabled=true;
+    document.getElementById('btn-11').disabled=true;
+    document.getElementById('btn-12').disabled=true;
+  
+  
+}
+function firstquiz12(){
+    document.getElementById('btn-12').style.background="rgb(248, 68, 68)";
+    document.getElementById('btn-9').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-9').disabled=true;
+    document.getElementById('btn-10').disabled=true;
+    document.getElementById('btn-11').disabled=true;
+    document.getElementById('btn-12').disabled=true;
+  
+}
+
+// ------------------------fourthquestion-------------------
+function firstquiz13(){
+   
+    document.getElementById('btn-13').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-16').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-13').disabled=true;
+    document.getElementById('btn-14').disabled=true;
+    document.getElementById('btn-15').disabled=true;
+    document.getElementById('btn-16').disabled=true;
+    
+
+}
+function firstquiz14(){
+
+    document.getElementById('btn-14').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-16').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-13').disabled=true;
+    document.getElementById('btn-14').disabled=true;
+    document.getElementById('btn-15').disabled=true;
+    document.getElementById('btn-16').disabled=true;
+ 
+}
+function firstquiz15(){
+
+    document.getElementById('btn-15').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-16').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-13').disabled=true;
+    document.getElementById('btn-14').disabled=true;
+    document.getElementById('btn-15').disabled=true;
+    document.getElementById('btn-16').disabled=true;
+  
+  
+}
+function firstquiz16(){
+    document.getElementById('btn-16').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-13').disabled=true;
+    document.getElementById('btn-14').disabled=true;
+    document.getElementById('btn-15').disabled=true;
+    document.getElementById('btn-16').disabled=true;
+    document.getElementById('scores').textContent=score++;
+
+}
+
+
+// ----------------------five question-----------------------
+function firstquiz17(){
+   
+
+    document.getElementById('btn-17').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-19').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-17').disabled=true;
+    document.getElementById('btn-18').disabled=true;
+    document.getElementById('btn-19').disabled=true;
+    document.getElementById('btn-20').disabled=true;
+    
+ 
+}
+function firstquiz18(){
+
+    document.getElementById('btn-18').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-19').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-17').disabled=true;
+    document.getElementById('btn-18').disabled=true;
+    document.getElementById('btn-19').disabled=true;
+    document.getElementById('btn-20').disabled=true;
+ 
+}
+function firstquiz19(){
+
+    document.getElementById('btn-19').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-17').disabled=true;
+    document.getElementById('btn-18').disabled=true;
+    document.getElementById('btn-19').disabled=true;
+    document.getElementById('btn-20').disabled=true;
+  
+        document.getElementById('scores').textContent=score++;
+  
+}
+function firstquiz20(){
+
+    document.getElementById('btn-20').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-19').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-17').disabled=true;
+    document.getElementById('btn-18').disabled=true;
+    document.getElementById('btn-19').disabled=true;
+    document.getElementById('btn-20').disabled=true;
+  
+}
+
+// ------------------------------six-----------------------
+
+function firstquiz21(){
+   
+    document.getElementById('btn-21').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-24').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-21').disabled=true;
+    document.getElementById('btn-22').disabled=true;
+    document.getElementById('btn-23').disabled=true;
+    document.getElementById('btn-24').disabled=true;
+    
+
+}
+function firstquiz22(){
+
+    document.getElementById('btn-22').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-24').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-21').disabled=true;
+    document.getElementById('btn-22').disabled=true;
+    document.getElementById('btn-23').disabled=true;
+    document.getElementById('btn-24').disabled=true;
+ 
+}
+function firstquiz23(){
+
+    document.getElementById('btn-23').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-24').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-21').disabled=true;
+    document.getElementById('btn-22').disabled=true;
+    document.getElementById('btn-23').disabled=true;
+    document.getElementById('btn-24').disabled=true;
+  
+  
+}
+function firstquiz24(){
+    document.getElementById('btn-24').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-21').disabled=true;
+    document.getElementById('btn-22').disabled=true;
+    document.getElementById('btn-23').disabled=true;
+    document.getElementById('btn-24').disabled=true;
+    document.getElementById('scores').textContent=score++;
+
+}
+
+// ----------------------------------seven-----------------------------------
+function firstquiz25(){
+   
+
+    document.getElementById('btn-25').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-27').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-25').disabled=true;
+    document.getElementById('btn-26').disabled=true;
+    document.getElementById('btn-27').disabled=true;
+    document.getElementById('btn-28').disabled=true;
+    
+ 
+}
+function firstquiz26(){
+
+    document.getElementById('btn-26').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-27').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-25').disabled=true;
+    document.getElementById('btn-26').disabled=true;
+    document.getElementById('btn-27').disabled=true;
+    document.getElementById('btn-28').disabled=true;
+ 
+}
+function firstquiz27(){
+
+    document.getElementById('btn-27').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-25').disabled=true;
+    document.getElementById('btn-26').disabled=true;
+    document.getElementById('btn-27').disabled=true;
+    document.getElementById('btn-28').disabled=true;
+  
+        document.getElementById('scores').textContent=score++;
+  
+}
+function firstquiz28(){
+
+    document.getElementById('btn-28').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-27').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-25').disabled=true;
+    document.getElementById('btn-26').disabled=true;
+    document.getElementById('btn-27').disabled=true;
+    document.getElementById('btn-28').disabled=true;
+  
+}
+
+// --------------------------eight---------------------------
+function firstquiz29(){
+   
+
+    document.getElementById('btn-29').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-30').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-29').disabled=true;
+    document.getElementById('btn-30').disabled=true;
+    document.getElementById('btn-31').disabled=true;
+    document.getElementById('btn-32').disabled=true;
+    
+ 
+}
+function firstquiz30(){
+
+    document.getElementById('btn-30').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-31').disabled=true;
+    document.getElementById('btn-32').disabled=true;
+    document.getElementById('btn-28').disabled=true;
+    document.getElementById('btn-29').disabled=true;
+    document.getElementById('scores').textContent=score++;
+
+}
+function firstquiz31(){
+
+    document.getElementById('btn-31').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-30').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-29').disabled=true;
+    document.getElementById('btn-30').disabled=true;
+    document.getElementById('btn-31').disabled=true;
+    document.getElementById('btn-32').disabled=true;
+  
+  
+}
+function firstquiz32(){
+
+    document.getElementById('btn-32').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-30').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-29').disabled=true;
+    document.getElementById('btn-30').disabled=true;
+    document.getElementById('btn-31').disabled=true;
+    document.getElementById('btn-32').disabled=true;
+  
+}
+
+// ---------------------nine-----------------
+
+function firstquiz33(){
+   
+
+    document.getElementById('btn-33').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-35').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-33').disabled=true;
+    document.getElementById('btn-34').disabled=true;
+    document.getElementById('btn-35').disabled=true;
+    document.getElementById('btn-36').disabled=true;
+    
+ 
+}
+function firstquiz34(){
+
+    document.getElementById('btn-34').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-35').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-33').disabled=true;
+    document.getElementById('btn-34').disabled=true;
+    document.getElementById('btn-35').disabled=true;
+    document.getElementById('btn-36').disabled=true;
+ 
+}
+function firstquiz35(){
+
+    document.getElementById('btn-35').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-36').disabled=true;
+    document.getElementById('btn-35').disabled=true;
+    document.getElementById('btn-33').disabled=true;
+    document.getElementById('btn-34').disabled=true;
+  
+        document.getElementById('scores').textContent=score++;
+  
+}
+function firstquiz36(){
+
+    document.getElementById('btn-36').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-35').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-33').disabled=true;
+    document.getElementById('btn-34').disabled=true;
+    document.getElementById('btn-35').disabled=true;
+    document.getElementById('btn-36').disabled=true;
+  
+}
+// --------------------------------ten------------------------
+function firstquiz37(){
+   
+
+    document.getElementById('btn-37').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-38').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-37').disabled=true;
+    document.getElementById('btn-38').disabled=true;
+    document.getElementById('btn-39').disabled=true;
+    document.getElementById('btn-40').disabled=true;
+    
+ 
+}
+function firstquiz38(){
+
+    document.getElementById('btn-38').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-37').disabled=true;
+    document.getElementById('btn-38').disabled=true;
+    document.getElementById('btn-39').disabled=true;
+    document.getElementById('btn-40').disabled=true;
+    document.getElementById('scores').textContent=score++;
+
+}
+function firstquiz39(){
+
+    document.getElementById('btn-39').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-38').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-37').disabled=true;
+    document.getElementById('btn-38').disabled=true;
+    document.getElementById('btn-39').disabled=true;
+    document.getElementById('btn-40').disabled=true;
+  
+  
+}
+function firstquiz40(){
+
+    document.getElementById('btn-40').style.background=" rgb(248, 68, 68)";
+    document.getElementById('btn-38').style.background="rgb(97, 242, 97)";
+    document.getElementById('btn-37').disabled=true;
+    document.getElementById('btn-38').disabled=true;
+    document.getElementById('btn-39').disabled=true;
+    document.getElementById('btn-40').disabled=true;
+  
+}
+
+
+
+// --------------------------------score-------------
+function showscore(){
+    
+    document.getElementById('score').style.display="";
+    document.getElementById('bada').style.display="none";
+   
+
+}  
